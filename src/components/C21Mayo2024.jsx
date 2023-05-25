b2a4846ed62bf4acfa66fb87619326b24a4a784b
jonpena <gran_200014@hotmail.com>
2023-05-24 20:50:27 -0400

    autobus resize

--- src/components/C21Mayo2024.jsx
+++ src/components/C21Mayo2024.jsx
@@ -3,9 +3,10 @@ import React from "react";
 
 const C21Mayo2024 = () => {
   return (
-    <div className="relative bottom-2">
-      <div className="max-w-screen-9xl w-full">
+    <div className="relative">
+      <div className="max-w-screen-9xl">
         <Image
+          className="7xl:h-[936px] object-cover aspect-video"
           src="/assets/ankai-mexico-camion-electrico.jpg"
           width={2590}
           height={1440}
@@ -15,7 +16,7 @@ const C21Mayo2024 = () => {
       <div className="absolute text-18xl text-gray-200 bg-[rgba(0,0,0,0.85)] pl-14 pr-16 py-14 top-36 right-0 z-20">
         <p className="leading-4">Los</p>
         <p className="leading-4 mt-10 ">Camiones</p>
-        <p className="leading-4 mt-10">Asiáticos</p>
+        <p className="leading-4 mt-10">Asiático</p>
         <p className="leading-4 mt-10">Líderes</p>
         <p className="leading-4 mt-10">en México</p>
       </div>
